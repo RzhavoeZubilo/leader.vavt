f56af91f3ee473ee71187f4b6ed31a3179540f8c
Денис Шкуропат <denshkuropat@gmail.com>
2022-11-22 02:27:04 +0300

    <Блок Избранные проекты>

---
+++ blocks/vavt_fav_project/db/access.php
@@ -0,0 +1,26 @@
+<?php
+$capabilities = array(
+
+    'block/vavt_fav_project:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/vavt_fav_project:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
---
+++ blocks/vavt_fav_project/lang/ru/block_vavt_fav_project.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['vavt_fav_project'] = 'Избранное Проекты';
+$string['pluginname'] = 'Избранное Проекты';
+$string['blocktitle'] = 'Заголовок';
+$string['vavt_fav_project:addinstance'] = 'Добавить блок Избранное Проекты';
+$string['vavt_fav_project:myaddinstance'] = 'Add a vavt_fav_project block to the My Moodle page';
